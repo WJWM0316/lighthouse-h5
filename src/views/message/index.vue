13e41723c52cb7566976c2f2875b9b9989ea1300
qiuhuanran <qiuhuanran@qq.com>
2018-01-04 11:16:22 +0800

    导航

---
+++ src/views/message/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>消息列表</div>
+</template>
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+import CommunityCard from '@/components/communityCard'
+
+@Component({
+  name: 'message-index',
+  components: {
+    CommunityCard
+  }
+})
+export default class HomeIndex extends Vue {
+
+}
+</script>
+
+<style lang="less" scoped>
+.p-home-index {
+
+}
+</style>
