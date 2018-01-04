282b8f7d150512ed5666b33fd15b3f7428181885
xjliu@thetiger.com.cn <xjliu@thetiger.com.cn>
2018-01-04 18:26:57 +0800

    commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,11 @@
 // 注册全局组件
 import Vue from 'vue'
+import VueScroller from 'vue-scroller'
+
 import Image from './imageItem'
+import BottomLoading from './bottomLoading'
+
+Vue.use(VueScroller)
 
 Vue.use(Image)
+Vue.use(BottomLoading)
