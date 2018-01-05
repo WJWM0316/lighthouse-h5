24b52a38b1d1faf960466eacff59b60f660154ab
rosary <heartmean@163.com>
2018-01-05 14:09:03 +0800

    朋友圈组件 (complete)

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import homes from './home'
 import centers from './center'
 import messages from './message'
+import page from './page'
 import store from './../store'
 // 请求接口
 // import {isLoginApi} from '@/api/page/account'
@@ -14,6 +15,7 @@ const routes = [
   ...homes, // 首页
   ...centers, // 我的
   ...messages, // 消息
+  ...page
 ]
 
 const router = new Router({
