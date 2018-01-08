2df0fb359a69db64760b1cb0dab80281f8b37632
qiuhuanran <qiuhuanran@qq.com>
2018-01-08 11:16:44 +0800

    调整

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const settings = {
 // 全局设置
-  host: process.env.NODE_ENV !== 'production' ? '/lighthouse_wap' : '/zikeserver', // 接口根地址。线上使用的是Nginx代理
+  host: process.env.NODE_ENV !== 'production' ? '/lighthouse_wap' : '/lighthouse_wap', // 接口根地址。线上使用的是Nginx代理
   dbPrefix: 'lighthourse_wap_', // 本地存储的key
   productUrl: 'https://light.house.api.ziwork.com',
   devUrl: 'https://light.house.api.ziwork.com',
