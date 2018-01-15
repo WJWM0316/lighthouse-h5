f716fe53996dba0757775f9bf48365beaf38f34e
qiuhuanran <qiuhuanran@qq.com>
2018-01-15 19:22:32 +0800

    调整

--- src/api/pages/pay.js
+++ src/api/pages/pay.js
@@ -3,7 +3,7 @@ import {request} from '../require.js'
 // 支付接口
 export const payApi = (data) => {
   return request({
-    url: '/wap/pay/AppletApiPay',
+    url: '/wap/pay/JsApiPay',
     data
   })
 }
