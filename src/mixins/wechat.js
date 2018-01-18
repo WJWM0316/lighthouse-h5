ca48a0a2683b8a4a591433a91e9512b60b45ac00
xjliu@thetiger.com.cn <xjliu@thetiger.com.cn>
2018-01-18 15:53:50 +0800

    commit

--- src/mixins/wechat.js
+++ src/mixins/wechat.js
@@ -65,13 +65,6 @@ export default {
           sizeType: options.sizeType || ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
           sourceType: options.sourceType || ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
           success: async function (res) {
-            res.localDatas = []
-            if (self.$wechat.getLocalImgData) {
-              await res.localIds.forEach(async localId => {
-                const { localData } = await self.wechatGetLocalImgData(localId)
-                res.localDatas.push(localData)
-              })
-            }
             resolve(res)
             // const localIds = res.localIds // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
           },
@@ -88,16 +81,20 @@ export default {
      */
     wechatGetLocalImgData(localId) {
       return new Promise((resolve, reject) => {
-        this.$wechat.getLocalImgData({
-          localId: localId, // 图片的localID
-          success: function (res) {
-            resolve(res)
-            // const localData = res.localData // localData是图片的base64数据，可以用img标签显示
-          },
-          fail: function (e) {
-            reject(e)
-          }
-        })
+        if (this.$wechat.getLocalImgData) {
+          this.$wechat.getLocalImgData({
+            localId: localId, // 图片的localID
+            success: function (res) {
+              resolve(res)
+              // const localData = res.localData // localData是图片的base64数据，可以用img标签显示
+            },
+            fail: function (e) {
+              reject(e)
+            }
+          })
+        } else {
+          resolve({ localData: '' })
+        }
       })
     },
 
