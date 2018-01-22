898fd0efed46ff9e9c8b4d62565fedf89aae4e49
qiuhuanran <qiuhuanran@qq.com>
2018-01-22 18:41:11 +0800

    测试服

---
+++ src/views/center/create.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+  name: 'center-editinfo',
+  components: {
+  },
+})
+export default class CenterEditinfo extends Vue {
+}
+</script>
+
+<style lang="less" scoped type="text/less">
+@import "../../styles/variables";
+@import "../../styles/mixins";
+
+</style>
+
