c2da68e25b6e6b23d19ea0003286223095545cad
qiuhuanran <qiuhuanran@qq.com>
2018-01-24 13:59:14 +0800

    v1.1

--- src/views/center/create.vue
+++ src/views/center/create.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="p-body p-center-create">
     <group label-width="400px" label-margin-right="10" label-align="left" class="p-group">
-      <popup-picker title="灯塔类别" placeholder="请选择灯塔类型" :data="lightHouseType" v-model="params.type"
+      <popup-picker class="" title="灯塔类别" placeholder="请选择灯塔类型" :data="lightHouseType" v-model="params.type"
                     value-text-align="left"></popup-picker>
-      <x-input class="margintop50" title="一句话介绍" placeholder="例如：走向百万年薪之类" v-model="params.introduce"></x-input>
+      <x-input class="margintop50" title="一句话介绍" placeholder="例如：走向百万年薪之类"
+               :showClear="false"
+               v-model="params.introduce"></x-input>
       <cell title="你的优势" class="margintop50 noboder"></cell>
-      <x-textarea placeholder="请尽量说明您的优势，例如您有怎样的行业或项
-目经历？获得过什么样的殊荣？" :show-counter="false" :rows="3" v-model="params.value"></x-textarea>
-      <cell title="灯塔价值" class="margintop50 noboder"></cell>
-      <x-textarea placeholder="您想向学员传输什么样的话题或内容？学员都可
-以获得什么？" :show-counter="false" :rows="3" v-model="params.advantage"></x-textarea>
+      <textarea class="common-textarea" placeholder="请尽量说明您的优势，例如您有怎样的行业或项目经历？获得过什么样的殊荣？"
+                :rows="4" v-model="params.value" maxlength="1000"></textarea>
+      <cell title="灯塔价值" class="margintop50 noboder nobefore"></cell>
+      <textarea class="common-textarea" placeholder="您想向学员传输什么样的话题或内容？学员都可以获得什么？"
+                :rows="4" v-model="params.advantage" maxlength="1000"></textarea>
     </group>
+    <div class="agree-container">确定创建则代表您已确认并同意<span class="lighthouse-agree">《创建灯塔协议》</span></div>
+    <div class="btn-group">
+      <div class="create-btn">确定创建灯塔</div>
+    </div>
   </div>
 </template>
 
@@ -45,15 +51,58 @@
   @import "../../styles/mixins";
 
   .p-center-create {
-    font-size: 10px;
+    box-sizing: content-box;
+    margin-right: 100px;
+    width: 100%;
+
+    .weui-cells {
+      font-size: 15px;
+    }
     .weui-cells:before, .weui-cells:after {
       border: 1px solid transparent;
     }
-    .noboder{
+    .noboder {
       border-bottom: transparent;
     }
-    .margintop50{
-      padding-top: 20px;
+    .nobefore:before {
+      border: transparent;
+    }
+    .margintop50 {
+      padding-top: 25px;
+    }
+    .p-group {
+      .mr15 {
+        margin-right: 15px;
+      }
+      .common-textarea {
+        border-radius: 3px;
+        margin-left: 15px;
+        padding: 15px;
+        width: 310px;
+        border: 1px solid #ededed;
+        background: #fbfbfb;
+      }
+    }
+    .agree-container {
+      color: #bcbcbc;
+      margin-top: 15px;
+      margin-left: 15px;
+      .lighthouse-agree {
+        color: #d7ab70;
+      }
+    }
+    .btn-group {
+      border-top: 1px solid #ededed;
+      padding: 12px 20px;
+      margin-top: 13px;
+      .create-btn {
+        border-radius: 22px;
+        background: #ffe266;
+        width: 100%;
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+      }
     }
   }
 </style>
