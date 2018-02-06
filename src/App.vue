64753bff33a26342f17f9251e0857edace33a4dc
rosary <heartmean@163.com>
2018-02-06 22:04:36 +0800

    分享Logo (fix)

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,7 @@ import WechatMixin from '@/mixins/wechat'
             'titles': '小灯塔|职场导师知识分享社区|照亮你职场的路',
             'title': '小灯塔|职场导师知识分享社区|照亮你职场的路',
             'desc': '名师高徒，社群化训练和学习！职场人脉，吸收大咖进阶干货！',
-            'imgUrl': 'https://cdnstatic.zike.com/Uploads/static/beacon/head-banner.png',
+            'imgUrl': 'https://cdnstatic.zike.com/Uploads/static/beacon/lighthouse-logo.png',
             'link': location.origin + '/beaconweb/#/'
           })
         }
