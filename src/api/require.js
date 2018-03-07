61224f70da3e6fc4f74fd0fafeb878eeb0c3d2f4
qiuhuanran <qiuhuanran@qq.com>
2018-03-07 19:03:32 +0800

    登录调整

--- src/api/require.js
+++ src/api/require.js
@@ -80,7 +80,9 @@ export const request = ({type = 'post', url, data = {}, config = {}} = {}) => {
         })
       }
       if (data && data.statusCode === 431) { // 需要授权
-        location.href = `${settings.serverUrl}/wap/wechat/snsapiUserinfo?zike_from=${location.href}`
+        const hashParams = location.hash.substring(1)
+        const hostname = location.href.split('?')[0]
+        location.href = `${settings.serverUrl}/wap/wechat/snsapiUserinfo?zike_from=${hostname}&key=${hashParams}`
         return data.data === undefined ? {} : data.data
       }
       if (data && data.statusCode === 264) { // 内容找不到
