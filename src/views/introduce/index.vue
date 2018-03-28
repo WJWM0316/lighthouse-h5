694fab94f61d788159dc384a78208235f91bd246
xjliu@thetiger.com.cn <xjliu@thetiger.com.cn>
2018-03-28 16:49:24 +0800

    commit

--- src/views/introduce/index.vue
+++ src/views/introduce/index.vue
@@ -1,7 +1,7 @@
 <template>
 
   <!-- 大咖介绍页 -->
-  <div class="p-body big-shot-introduce">
+  <div class="p-body big-shot-introduce" :class="{ 'no-pdb': !completelyShow }">
 
     <div class="container" ref="big-shot-introduce-container">
       <div class="header">
@@ -402,10 +402,15 @@
 <style lang="less" scoped type="text/less">
   .big-shot-introduce {
     padding-bottom: 55px;
-    /*height: 100%;*/
+    height: auto;
+    min-height: 100%;
     /*display: flex;*/
     /*flex-flow: column nowrap;*/
-    /*overflow: hidden;*/
+    overflow: hidden;
+
+    &.no-pdb {
+      padding-bottom: 0;
+    }
 
     & .header {
       position: relative;
