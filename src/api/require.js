b7789a1441de1655ff982626fd54afbbb2ec4327
rosary <heartmean@163.com>
2018-03-28 12:02:12 +0800

    (fix)

--- src/api/require.js
+++ src/api/require.js
@@ -79,7 +79,7 @@ export const request = ({type = 'post', url, data = {}, config = {}} = {}) => {
       if (data && data.statusCode === 433) { // 支付接口没有登录权限,跳去手机号登录
         store.dispatch('remove_userinfo')
         hideLoading(globalLoading)
-        if (location.href.endsWith('reload=true')) {
+        if (location.href.split('?').length === 3) {
           router.replace({
             name: 'login',
             query: {redirect: encodeURI(location.href + '&autoPay=true')}
