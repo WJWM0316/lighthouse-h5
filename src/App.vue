9fbf3815cc2844eedc1fade023d0466e0da1281a
ChenHaoBin <531120903@qq.com>
2018-05-02 21:19:45 +0800

    最终版提交0502

--- src/App.vue
+++ src/App.vue
@@ -79,8 +79,8 @@ import WechatMixin from '@/mixins/wechat'
         if (customSharePageName.indexOf(route.name) < 0) {
           // 页面分享信息
           this.wechatShare({
-            'titles': '小灯塔|职场知识分享社区|照亮你职场的路',
-            'title': '小灯塔|职场知识分享社区|照亮你职场的路',
+            'titles': '小灯塔|互联网职场学习社区|照亮你职场的路',
+            'title': '小灯塔|互联网职场学习社区|照亮你职场的路',
             'desc': '名师高徒，社群化训练和学习！职场人脉，吸收大咖进阶干货！',
             'imgUrl': 'https://cdnstatic.zike.com/Uploads/static/beacon/lighthouse-logo.png',
             'link': location.origin + '/beaconweb/#/'
