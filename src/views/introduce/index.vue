efb653a91a614c32e474c4782226db81eea957de
DESKTOP-LRMLL2E\42902 <wwx@theger.com.cn>
2018-05-24 15:34:15 +0800

    分销随机二维码

--- src/views/introduce/index.vue
+++ src/views/introduce/index.vue
@@ -322,6 +322,7 @@
           if (res.err_msg === 'get_brand_wcpay_request:ok') {
             self.$vux.toast.text('已购买成功', 'bottom')
             const { communityId } = self.$route.params
+            let number = Math.random() * 8 + 1
             console.log('communityId', communityId)
             //
             switch (communityId) {
@@ -362,7 +363,7 @@
                 self.$store.dispatch('show_qr', {type: 2})
                 break
               case '85f224b0c5afa3f85fc780dc486bf0b8': // 测试专用
-                self.$store.dispatch('show_qr', {type: 2})
+                self.$store.dispatch('show_qr', {type: number})
                 break
               default:
                 location.reload()
