8bf5225d9fbab77459ade3650ce220eabad398af
DESKTOP-LRMLL2E\42902 <wwx@theger.com.cn>
2018-05-28 18:23:39 +0800

    发布内容 文案修改

--- src/views/introduce/community.vue
+++ src/views/introduce/community.vue
@@ -100,7 +100,7 @@
           <span class="desc"><img src="../../assets/icon/bnt_askquestion@3x.png"/>回答问题<i class="answer-count _" v-if=" pageInfo['answerTotal']> 0">{{pageInfo['answerTotal']}}</i></span>
         </button>
         <!--<button @click="release"><img src="../../assets/icon/bnt_post@3x.png"/>发布动态</button>-->
-        <button @click="release"><img src="../../assets/icon/bnt_post@3x.png"/>发布动态</button>
+        <button @click="release"><img src="../../assets/icon/bnt_post@3x.png"/>发布内容</button>
       </div>
       <div class="ask-warp" v-else>
       <!--<div class="ask-btn" @click="askBtnClick" v-else>-->
@@ -111,7 +111,7 @@
         <button @click="postQuestions" v-if="this.pageInfo.isCourse===1">
           <span class="desc"><img src="../../assets/icon/bnt_askquestion@3x.png"/>我要提问</span>
         </button>
-        <button @click="posted" class="post-tip" v-if="isKayo=='manager' && type===1"><img src="../../assets/icon/bnt_post@3x.png"/>发布动态</button>
+        <button @click="posted" class="post-tip" v-if="isKayo=='manager' && type===1"><img src="../../assets/icon/bnt_post@3x.png"/>发布内容</button>
         <button @click="posted" class="post-tip" v-else><img src="../../assets/icon/bnt_post@3x.png"/>发帖子</button>
       </div>
     </div>
