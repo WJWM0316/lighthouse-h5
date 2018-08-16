1986aaf09f0a73749f756a059dea4989ed5b693b
ChenHaoBin <531120903@qq.com>
2018-08-16 17:11:51 +0800

    提交0816

--- src/components/lessondynamicItem/lessondynamicItem.vue
+++ src/components/lessondynamicItem/lessondynamicItem.vue
@@ -110,7 +110,7 @@
       </div>
 
       <!-- 评论区 -->
-      <div class="comment-area" v-if="!hideCommentArea && (item.favorTotal > 0 || (item.commentTotal > 0 && item.commentlist && item.commentlist.hotComments.length > 0))">
+      <div class="comment-area" v-if="!hideCommentArea && (item.favorTotal > 0 || (item.commentTotal > 0 && item.commentlist.hotComments && item.commentlist.hotComments.length > 0))">
         <!-- 点赞信息 -->
           <div class="praise-block" v-if="item.favorTotal > 0">
             <img class="icon-zan" src="./../../assets/icon/bnt_zan@3x.png" />
@@ -122,7 +122,7 @@
 
         <!-- 评论信息 -->
 
-        <div class="reply-block" v-if="item.commentTotal > 0 && item.commentlist && item.commentlist.hotComments.length > 0">
+        <div class="reply-block" v-if="item.commentTotal > 0 && item.commentlist.hotComments && item.commentlist.hotComments.length > 0">
           <template>
             <div class="hot-reply">
               <div class="hot-reply-icon">热门评论</div> 
