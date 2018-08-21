90ad7856415864caf9ee2acf8a07dd03ec5d2af1
ChenHaoBin <531120903@qq.com>
2018-08-21 16:46:18 +0800

    合并鑫科代码

--- src/views/introduce/punchList.vue
+++ src/views/introduce/punchList.vue
@@ -1,19 +1,23 @@
 <template>
 	<!--打卡列表页-->
 	<div class="PunchList">
-		<lessondynamicItem
-		 v-for="(item, index) in CourseCardList"
-		 :key = "index"
-		 :item="item"
-         :showDelBtn="true"
-         :communityId="communityId"
-         :isFold="true"
-         :isNeedHot="true"
-         :hideBorder="false"
-         :isLesson="true"
-         :disableContentClick="false"
-         @disableOperationEvents="operation"
-      ></lessondynamicItem>
+		<scroll  :pullupable="false" :refreshable="false" :infinite-scroll="true" @infinite-scroll="handlePullup" :is-none-data="CourseCardList.length===listLength" :show-bottom-loading='true'>
+			<div>
+				<lessondynamicItem
+				 v-for="(item, index) in CourseCardList"
+				 :key = "index"
+				 :item="item"
+		         :showDelBtn="true"
+		         :communityId="communityId"
+		         :isFold="true"
+		         :isNeedHot="true"
+		         :hideBorder="false"
+		         :isLesson="true"
+		         :disableContentClick="false"
+		         @disableOperationEvents="operation"
+		      ></lessondynamicItem>
+				</div>
+	    </scroll>
 	</div>
 </template>
 
@@ -23,15 +27,18 @@
 	import lessondynamicItem from '@/components/lessondynamicItem/lessondynamicItem'
 	import { getCourseCardListApi } from '@/api/pages/pageInfo'
 	import moment from 'moment'
+	import Scroll from '@/components/scroller'
 	@Component({
 		name:'punch-list',
 		components: {
-      lessondynamicItem
+      lessondynamicItem,
+      Scroll
     },
 	})
 	export default class punchList extends Vue {
 		communityId = ""
 		CourseCardList=""
+		listLength = ""
 		page = 0 	//当前页码
 		created(){
 			this.communityId = this.$route.query.communityId
@@ -48,6 +55,10 @@
 				}else{
 					this.CourseCardList =res.peopleCourseCardList
 				}
+				this.listLength = res.totalCount
+			}).catch(res=>{
+				console.log(res,"报错信息。")
+				this.$vux.toast.text('加载失败，请重试', 'bottom')
 			})
 		}
 		
@@ -106,6 +117,36 @@
 				}
 			})
 		}
+	  
+	  //上拉事件
+	  async handlePullup (loaded) {
+			if(this.CourseCardList.length===this.listLength){
+				loaded('ended')
+				return
+			}else{
+				this.page+=1;
+				let param={
+					page:this.page,
+					pageCount:20,
+					communityId:this.$route.query.communityId,
+	  			courseId:this.$route.query.courseId,
+	  			type:this.$route.query.toList==="all"?2:1,
+				}
+				
+				this.getCourseCardListApi(param).then(res=>{
+					if(this.$route.query.toList === "excellent"){
+						this.CourseCardList = [...this.CourseCardList,...res.excellentPeopleCourseCardList]
+					}else{
+						this.CourseCardList =[...this.CourseCardList,...res.peopleCourseCardList]
+					}
+					loaded('done')
+				}).catch(res=>{
+					loaded('done')
+					this.$vux.toast.text('加载失败，请重试', 'bottom')
+				})
+				
+			}
+    }
 		
 	}
 </script>
