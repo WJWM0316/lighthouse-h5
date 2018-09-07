c82b1784e3f3a4a639b3496fec1adafbafba11e7
tangxinke <807685205@qq.com>
2018-09-07 18:33:29 +0800

    no message

--- src/views/activity/teacher/teacherActivity.vue
+++ src/views/activity/teacher/teacherActivity.vue
@@ -300,9 +300,9 @@ import { getBeaconsApi } from '@/api/pages/home'
         function (res) {
           // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
           if (res.err_msg === 'get_brand_wcpay_request:ok') {
-            self.$vux.toast.text(`已购买成功${self.selectItem.isCourse}${self.selectItem.communityId}`, 'bottom')
+            self.$vux.toast.text(`已购买成功${self.selectItem.isCourse}-${self.selectItem.communityId}`, 'bottom')
 
-            if(self.selectItem.community == this.allBuyCommity){
+            if(self.selectItem.communityId == this.allBuyCommity){
               self.$vux.toast.text(`allby`, 'bottom')
               this.toPay = true
             }else {
