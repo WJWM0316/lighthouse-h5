d1e50b800738f5af9f265d410f6b0fbbb0e7de3c
ChenHaoBin <531120903@qq.com>
2018-12-11 15:31:39 +0800

    测试复制功能

--- src/components/communityCard_v2/CommunityCard.vue
+++ src/components/communityCard_v2/CommunityCard.vue
@@ -74,30 +74,18 @@
       </template>
     </div>
     <!--训练营客服弹窗-->
-    <div class="trainingCampAlert" v-if="trainingCampAlert" @click.stop="close">
-      <div class="content" v-if="community.alterWechatQrcode" @click.stop="">
-        <img class="closeBtn" src="../../assets/icon/icon-close.png" alt="" @click.stop="close" />
-        <h3>联系客服</h3>
-        <p>{{community.alterTxt}}</p>
-        <img :src="community.alterWechatQrcode" alt="" />
-        <span>长按保存二维码</span>
-        <div class="copy" @click.stop="copy($event)">复制微信号</div>
-      </div>
-      <div class="textContent" @click.stop="" v-else>
-        <img class="closeBtn" src="../../assets/icon/icon-close.png" alt="" @click.stop="close" />
-        <h3>恭喜你加入训练营</h3>
-        <span id="copy">请添加客服微信：{{community.consultantCustomerWechat}}</span>
-        <div class="copy" @click.stop="copy">复制微信号</div>
-      </div>
-    </div>
+    <popus :trainingCampAlert="trainingCampAlert" :pageInfo="community" :isKefu="true" @close="close"></popus>
   </a>
 </template>
 <script>
 import Vue from 'vue'
 import Component from 'vue-class-component'
-
+import popus from '@/components/Popups/Popups'
 @Component({
   name: 'community-card',
+  components: {
+    popus
+  },
   props: {
     // 大咖信息
     community: {
@@ -165,37 +153,6 @@ export default class CommunityCard extends Vue {
 	created(){
     console.log("我是触发的community",this.isEntentr);
 	}
-	/* 复制 */
-	copy () {
-	  let that = this
-    setTimeout(function () {
-      //要复制文字的节点
-      let text = that.community.consultantCustomerWechat
-      const input = document.createElement('input')
-      input.setAttribute('readonly', 'readonly')
-      input.setAttribute('value', text)
-      document.body.appendChild(input) 
-      //区分iPhone设备
-      if (navigator.userAgent.match(/(iPhone|iPod|iPad);?/i)) {
-        window.getSelection().removeAllRanges();//这段代码必须放在前面否则无效
-        let range = document.createRange()
-        // 选中需要复制的节点
-        range.selectNode(input)
-        // 执行选中元素
-        window.getSelection().addRange(range)
-        // 执行 copy 操作
-        let successful = document.execCommand('copy')
-        // 移除选中的元素
-        window.getSelection().removeAllRanges()
-      }else{
-        input.select() // 选择对象
-        document.execCommand("Copy") // 执行浏览器复制命令
-      }
-      document.body.removeChild(input)
-      that.$vux.toast.text('复制成功', 'bottom')
-      that.trainingCampAlert = false
-    }, 500)
-  }
 	close () {
     this.trainingCampAlert = false
   }
@@ -582,75 +539,6 @@ export default class CommunityCard extends Vue {
             height: 12px;
           }
         }
-      }
-    }
-  }
-  /* 训练营弹窗 */
-  .trainingCampAlert{
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 9999;
-    width: 100%;
-    height: 100vh;
-    background:rgba(0,0,0,0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .content, .textContent{
-      border-radius: 4px;
-      position: relative;
-      width: 280px;
-      height: 334px;
-      background: #FFFFFF;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      >.closeBtn{
-        position: absolute;
-        margin-top: 0;
-        top: 15px;
-        right: 15px;
-        width: 15px;
-        height: 15px;
-      }
-      h3{
-        font-size: 18px;
-        font-weight: 500;
-        color: #354048;
-      }
-      p{
-        padding: 0 25px;
-        margin-top: 12px;
-        font-size: 15px;
-        font-weight: 300;
-        color: #666666;
-      }
-      img{
-        margin-top: 13px;
-        width: 122px;
-        height: 122px;
-      }
-      span{
-        font-size: 13;
-        font-weight: 400;
-        color: #BCBCBC;
-        margin-top: 2px;
-      }
-      .copy{
-        font-size: 17px;
-        color: #D7AB70;
-        font-weight: 400;
-        margin-top: 30px;
-      }
-    }
-    .textContent{
-      height: 160px;
-      #copy{
-        color: #666666;
-        margin-top: 12px;
-        font-size: 15px;
       }
     }
   }
--- src/views/introduce/index_v2.vue
+++ src/views/introduce/index_v2.vue
@@ -147,22 +147,7 @@
       @hintSucFuc = hintSucFuc
     ></hint-msg>
     <!--加入训练营弹窗-->
-    <div class="trainingCampAlert" v-if="trainingCampAlert" @click.stop="close">
-      <div class="content" v-if="pageInfo.alterWechatQrcode" @click.stop="">
-        <img class="closeBtn" src="../../assets/icon/icon-close.png" alt="" @click.stop="close" />
-        <h3>恭喜你加入训练营</h3>
-        <p>{{pageInfo.alterTxt}}</p>
-        <img :src="pageInfo.alterWechatQrcode" alt="" />
-        <span>长按保存二维码</span>
-        <div class="copy" @click.stop="copy($event)">复制微信号</div>
-      </div>
-      <div class="textContent" @click.stop="" v-else>
-        <img class="closeBtn" src="../../assets/icon/icon-close.png" alt="" @click.stop="close" />
-        <h3>恭喜你加入训练营</h3>
-        <span id="copy">请添加客服微信：{{pageInfo.consultantCustomerWechat}}</span>
-        <div class="copy" @click.stop="copy">复制微信号</div>
-      </div>
-    </div>
+    <popus :trainingCampAlert="trainingCampAlert" :pageInfo="pageInfo" :isKefu="false" @close="close"></popus>
   </div>
 </template>
 <script>
@@ -177,6 +162,7 @@
   import {payApi, freePay} from '@/api/pages/pay'
   import ShareDialog from '@/components/shareDialog/ShareDialog'
   import payment from '@/components/payment/payment'
+  import popus from '@/components/Popups/Popups'
   Component.registerHooks([
 	  'beforeRouteEnter',
 	  'beforeRouteLeave',
@@ -191,7 +177,8 @@
       CommunityCard,
       hintMsg,
       ShareDialog,
-      payment
+      payment,
+      popus // 客服或加入弹窗
     },
     computed: {
       // 剩余免费名额
@@ -312,33 +299,6 @@
           that.freeJoin()
        }
        this.cloHint()
-    }
-    
-    copy () {
-      //要复制文字的节点
-      let text = this.pageInfo.consultantCustomerWechat
-      const input = document.createElement('input')
-      input.setAttribute('readonly', 'readonly')
-      input.setAttribute('value', text)
-      document.body.appendChild(input) 
-      //区分iPhone设备
-      if (navigator.userAgent.match(/(iPhone|iPod|iPad);?/i)) {
-        window.getSelection().removeAllRanges();//这段代码必须放在前面否则无效
-        let range = document.createRange()
-        // 选中需要复制的节点
-        range.selectNode(input)
-        // 执行选中元素
-        window.getSelection().addRange(range)
-        // 执行 copy 操作
-        let successful = document.execCommand('copy')
-        // 移除选中的元素
-        window.getSelection().removeAllRanges()
-      }else{
-        input.select() // 选择对象
-        document.execCommand("Copy") // 执行浏览器复制命令
-      }
-      document.body.removeChild(input)
-      this.$vux.toast.text('复制成功', 'bottom')
     }
     
     close () {
@@ -1069,75 +1029,6 @@
           height: 100%;
           vertical-align: middle;
           text-align: center;
-        }
-      }
-    }
-    /* 训练营弹窗 */
-    .trainingCampAlert{
-      position: fixed;
-      top: 0;
-      left: 0;
-      z-index: 9999;
-      width: 100%;
-      height: 100vh;
-      background:rgba(0,0,0,0.5);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      .content, .textContent{
-        border-radius: 4px;
-        position: relative;
-        width: 280px;
-        height: 334px;
-        background: #FFFFFF;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        >.closeBtn{
-          position: absolute;
-          margin-top: 0;
-          top: 15px;
-          right: 15px;
-          width: 15px;
-          height: 15px;
-        }
-        h3{
-          font-size: 18px;
-          font-weight: 500;
-          color: #354048;
-        }
-        p{
-          padding: 0 25px;
-          margin-top: 12px;
-          font-size: 15px;
-          font-weight: 300;
-          color: #666666;
-        }
-        img{
-          margin-top: 13px;
-          width: 122px;
-          height: 122px;
-        }
-        span{
-          font-size: 13;
-          font-weight: 400;
-          color: #BCBCBC;
-          margin-top: 2px;
-        }
-        .copy{
-          font-size: 17px;
-          color: #D7AB70;
-          font-weight: 400;
-          margin-top: 30px;
-        }
-      }
-      .textContent{
-        height: 160px;
-        #copy{
-          color: #666666;
-          margin-top: 12px;
-          font-size: 15px;
         }
       }
     }
